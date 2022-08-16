2637618358bb6d095e3fb5c48e14f45e9a1a88ef
markelpolo <42959011+markelpolo@users.noreply.github.com>
2022-08-16 02:02:40 -0500

    Added canvas setup

--- bump/bumpscript.js
+++ bump/bumpscript.js
@@ -1,4 +1,12 @@
 // Canvas setup
+const canvas = document.getElementbyId('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 500;
+
+let score = 0;
+let gameFrame = 0;
+ctx.font = '50px Georgia';
 
 //Touch interactivity
 

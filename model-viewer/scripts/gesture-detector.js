4b4530a395d7132fb20cd910c509518a646e1c13
markelpolo <andrew.c.markel@gmail.com>
2023-12-06 09:41:16 -0600

    Remove PA, update position

--- model-viewer/scripts/gesture-detector.js
+++ model-viewer/scripts/gesture-detector.js
@@ -96,14 +96,14 @@ AFRAME.registerComponent("gesture-detector", {
         
           // Calculating vectors based off of coordinates
           const P = [currentCenter[0] - currentPosition[0], currentCenter[1] - currentPosition[1]];
-          console.log(P);
+          //console.log(P);
           const F = [previousPosition[0] - currentPosition[0], previousPosition[1] - currentPosition[1]];
-          console.log(F); 
+          //console.log(F); 
           // Calculating Torque of the position change using cross product / magnitude of the original position vector
           torqueList.push( (P[0]*F[1] - P[1]*F[0]) / Math.sqrt(Math.pow(P[0],2) + Math.pow(P[1],2)) );
         } 
         
-        console.log(torqueList);
+        //console.log(torqueList);
         
         // Calculating the total torque to apply to the z axis rotation
         eventDetail.positionChange.z = torqueList.reduce((sum, torque) => sum + torque, 0);
